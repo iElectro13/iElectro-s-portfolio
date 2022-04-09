edfc0f01e4b498b9736b11bd38d78e8f9cefef47
iElectro <ejose.acc@gmail.com>
2022-04-08 22:19:51 -0500

    Welcome section responsive

--- src/components/sections/Welcome.jsx
+++ src/components/sections/Welcome.jsx
@@ -9,7 +9,7 @@ const Welcome = () => {
             className="bg-neutral-900 text-neutral-100 flex flex-col justify-center items-center"
         >
             <MainLayout>
-                <h2 className="text-center text-2xl">
+                <h2 className="text-center text-2xl sm:text-4xl md:text-5xl lg:text-6xl">
                     Hello, I'm{" "}
                     <span className="font-bold text-pink-700">
                         Enmanuel Pereira.
@@ -17,10 +17,10 @@ const Welcome = () => {
                     <br />
                     I'm full-stack developer.
                 </h2>
-                <button className="btn btn-ghost">
+                <a href="#about" className="btn btn-ghost transition-colors">
                     View my work
                     <FaArrowRight className="ml-2" />
-                </button>
+                </a>
             </MainLayout>
         </div>
     );
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MainLayout = ({ children }) => {
     return (
-        <div className="min-h-screen flex flex-col justify-center items-center w-80 gap-4">
+        <div className="min-h-screen flex flex-col justify-center items-center w-11/12 gap-4 sm:w-10/12 md:w-4/5 lg:w-3/5">
             {children}
         </div>
     );
