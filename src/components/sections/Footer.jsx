f384dec3a2c3830c7fa062577e90cd931ff90c32
iElectro <ejose.acc@gmail.com>
2022-04-10 23:22:46 -0500

    link to blank

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
         <footer className="flex flex-col justify-center items-center gap-4 bg-neutral-800">
             <div className="flex flex-col justify-center items-center gap-4 responsive-container py-10">
                 <div className="flex justify-center items-center gap-4">
-                    <a href="https://github.com/iElectro13">
+                    <a href="https://github.com/iElectro13" target="_blank">
                         <FaGithubSquare className="text-neutral-100 text-4xl cursor-pointer" />
                     </a>
-                    <a href="https://www.linkedin.com/in/ielectro/">
+                    <a target="_blank" href="https://www.linkedin.com/in/ielectro/">
                         <FaLinkedin className="text-neutral-100 text-4xl cursor-pointer" />
                     </a>
                     <MdOutlinePictureAsPdf className="text-neutral-100 text-4xl cursor-pointer" />
