f8d9091bfb6b80b52b01eeb44b009fb79e854f43
iElectro <ejose.acc@gmail.com>
2022-04-10 22:23:01 -0500

    Filter done

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import MainLayout from "../../layouts/MainLayout";
 import Project from "../cards/Project";
 import Title from "../text/Title";
 
 const Projects = () => {
+    const allRef = useRef();
+    const [filter, setFilter] = useState("all");
     const projects = [
         {
             url: "https://em-hechoamano.vercel.app/",
@@ -24,38 +26,73 @@ const Projects = () => {
             category: "fullstack react php",
         },
         {
-          url: "https://my-first-todo-app.vercel.app/",
-          img: "/assets/images/todo-project.png",
-          title: "Fullstack todo with Express and React",
-          category: "fullstack react",
-      },
+            url: "https://my-first-todo-app.vercel.app/",
+            img: "/assets/images/todo-project.png",
+            title: "Fullstack todo with Express and React",
+            category: "fullstack react express",
+        },
     ];
+    useEffect(() => {
+        if (filter === "all") {
+            allRef.current.focus();
+        }
+    }, [filter]);
     return (
         <div
             id="projects"
             className="w-full flex flex-col justify-center items-center"
+            onClick={(e) => e.target.id === "projects" && setFilter("all")}
         >
             <MainLayout>
                 <Title title="PROJECTS" />
                 <div className="flex flex-wrap w-full justify-center items-center gap-2">
-                    <button className="py-1 px-3 text-neutral-600 bg-neutral-200 hover:bg-pink-700 hover:text-neutral-100 text-center transition-colors focus:bg-pink-700 focus:text-neutral-100">
+                    <button
+                        onClick={() => setFilter("all")}
+                        className="py-1 px-3 text-neutral-600 bg-neutral-200 hover:bg-pink-700 hover:text-neutral-100 text-center transition-colors focus:bg-pink-700 focus:text-neutral-100"
+                        ref={allRef}
+                    >
                         ALL
                     </button>
-                    <button className="py-1 px-3 text-neutral-600 bg-neutral-200 hover:bg-pink-700 hover:text-neutral-100 text-center transition-colors focus:bg-pink-700 focus:text-neutral-100">
+                    <button
+                        onClick={() => setFilter("fullstack")}
+                        className="py-1 px-3 text-neutral-600 bg-neutral-200 hover:bg-pink-700 hover:text-neutral-100 text-center transition-colors focus:bg-pink-700 focus:text-neutral-100"
+                    >
                         FULL-STACK
                     </button>
-                    <button className="py-1 px-3 text-neutral-600 bg-neutral-200 hover:bg-pink-700 hover:text-neutral-100 text-center transition-colors focus:bg-pink-700 focus:text-neutral-100">
-                        DJANGO
+                    <button
+                        onClick={() => setFilter("php")}
+                        className="py-1 px-3 text-neutral-600 bg-neutral-200 hover:bg-pink-700 hover:text-neutral-100 text-center transition-colors focus:bg-pink-700 focus:text-neutral-100"
+                    >
+                        PHP
                     </button>
-                    <button className="py-1 px-3 text-neutral-600 bg-neutral-200 hover:bg-pink-700 hover:text-neutral-100 text-center transition-colors focus:bg-pink-700 focus:text-neutral-100">
+                    <button
+                        onClick={() => setFilter("react")}
+                        className="py-1 px-3 text-neutral-600 bg-neutral-200 hover:bg-pink-700 hover:text-neutral-100 text-center transition-colors focus:bg-pink-700 focus:text-neutral-100"
+                    >
                         REACT-JS
                     </button>
+                    <button
+                        onClick={() => setFilter("express")}
+                        className="py-1 px-3 text-neutral-600 bg-neutral-200 hover:bg-pink-700 hover:text-neutral-100 text-center transition-colors focus:bg-pink-700 focus:text-neutral-100"
+                    >
+                        EXPRESS
+                    </button>
+                    <button
+                        onClick={() => setFilter("javascript")}
+                        className="py-1 px-3 text-neutral-600 bg-neutral-200 hover:bg-pink-700 hover:text-neutral-100 text-center transition-colors focus:bg-pink-700 focus:text-neutral-100"
+                    >
+                        JAVASCRIPT
+                    </button>
+                </div>
 
-                    <div className="grid grid-cols-1 place-content-center place-items-center gap-4 md:grid-cols-2">
-                        {projects.map((project) => (
-                            <Project key={project.title} {...project} />
-                        ))}
-                    </div>
+                <div className="grid grid-cols-1 place-content-center place-items-center gap-4 sm:grid-cols-2">
+                    {projects.map(
+                        (project) =>
+                            (project.category.includes(filter) ||
+                                filter === "all") && (
+                                <Project key={project.title} {...project} />
+                            )
+                    )}
                 </div>
             </MainLayout>
         </div>
