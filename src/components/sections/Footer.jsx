d2229551ef480d059f5398e5676387077eafd198
iElectro <ejose.acc@gmail.com>
2022-04-10 23:20:41 -0500

    Link to my social media

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { FaGithubSquare, FaLinkedin, FaRegFilePdf } from "react-icons/fa";
-import {MdOutlinePictureAsPdf} from "react-icons/md";
+import { MdOutlinePictureAsPdf } from "react-icons/md";
 
 const Footer = () => {
     return (
         <footer className="flex flex-col justify-center items-center gap-4 bg-neutral-800">
             <div className="flex flex-col justify-center items-center gap-4 responsive-container py-10">
                 <div className="flex justify-center items-center gap-4">
-                    <FaGithubSquare className="text-neutral-100 text-4xl cursor-pointer" />
-                    <FaLinkedin className="text-neutral-100 text-4xl cursor-pointer" />
+                    <a href="https://github.com/iElectro13">
+                        <FaGithubSquare className="text-neutral-100 text-4xl cursor-pointer" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/ielectro/">
+                        <FaLinkedin className="text-neutral-100 text-4xl cursor-pointer" />
+                    </a>
                     <MdOutlinePictureAsPdf className="text-neutral-100 text-4xl cursor-pointer" />
                 </div>
                 <span className="text-neutral-100">
-                    Enmanuel Pereira (iElectro) <span className="text-pink-700">&copy; 2022</span>
+                    Enmanuel Pereira (iElectro){" "}
+                    <span className="text-pink-700">&copy; 2022</span>
                 </span>
             </div>
         </footer>
